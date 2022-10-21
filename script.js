ba555bd94133c12fada79f4aa500e78f90f81d6c
nanae <7a1021@gmail.com>
2022-10-22 03:17:40 +0900

    css追加

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ $(function () {
     $('button').on('click', function () {
         let snack = $('input').val();
         let parameter = {
-            apiky: snack
-
-
+            apiky: snack,
+            apiky: "guest",
+            format: "jsonp"
         }
         // Ajaxの書き方
 
-        $.ajax({  
+        $.ajax({
             url: 'https://sysbird.jp/toriko/api/', //アクセスするURL
             type: 'get', //post or get
             cache: false,        //cacheを使うか使わないかを設定
@@ -21,21 +21,8 @@ $(function () {
         })
             .done(function (response) {
                 //通信成功時の処理
-                console.log(response);
-                // 中身の取り出し方
-                // 配列名[要素番号]
-                console.log(response.results);
-                console.log(response.results[0]);
-                // console.log();results[0]
-                console.log(response.results[0].address1);
-                // DOMの操作　jquery append関数
-                // append は追加text　はその部分だけ（北谷町吉原　のみ）
-            
-                $('.result').append(response.results[0].address1)
-                $('.result').append(response.results[0].address2)
                 //成功したとき実行したいスクリプトを記載
             })
-
             .fail(function (xhr) {
                 //通信失敗時の処理
                 //失敗したときに実行したいスクリプトを記載
@@ -44,6 +31,42 @@ $(function () {
                 //通信完了時の処理
                 //結果に関わらず実行したいスクリプトを記載
             });
+
+
+
+
+
+
+
+
+        // $.ajax({
+        //     url: 'https://sysbird.jp/toriko/api/', //アクセスするURL
+        //     type: 'get', //post or get
+        //     cache: false,        //cacheを使うか使わないかを設定
+        //     dataType: 'json',     //data type script・xmlDocument・jsonなど
+        //     data: parameter,           //アクセスするときに必要なデータを記載      
+        // })
+        //     .done(function (response) {
+        //         //通信成功時の処理
+
+        //         // 中身の取り出し方
+        //         // 配列名[要素番号]
+
+        //         // console.log();results[0]
+
+        //         // DOMの操作　jquery append関数
+
+        //         //成功したとき実行したいスクリプトを記載
+        //     })
+
+        //     .fail(function (xhr) {
+        //         //通信失敗時の処理
+        //         //失敗したときに実行したいスクリプトを記載
+        //     })
+        //     .always(function (xhr, msg) {
+        //         //通信完了時の処理
+        //         //結果に関わらず実行したいスクリプトを記載
+        //     });
 
 
 
